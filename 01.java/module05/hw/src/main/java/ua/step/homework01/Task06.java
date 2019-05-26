fbe3f7806bd77bb809168061c36dcd46f8805efc
Maxim <maxshaptala@gmail.com>
2019-05-26 23:53:01 +0300

    Обновлен тест 06 для второго дз, исправлено задание для 08 задания, изменена целевая платформа на 9

--- 01.java/module05/hw/src/main/java/ua/step/homework01/Task06.java
+++ 01.java/module05/hw/src/main/java/ua/step/homework01/Task06.java
@@ -2,7 +2,10 @@ package ua.step.homework01;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Random;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Задание: Заполнить массив на 10 элементов случайными числами
@@ -22,6 +25,7 @@ import java.util.Random;
  *      -1 -> 9 индекс
  */
 public class Task06 {
+
     public static void main(String[] args) {
         // TODO: не менять стоки ниже - нобходимо для тестирования @see ua.step.homework01.TaskTest06
         long seed = args.length > 0 ? Long.parseLong(args[0]) : LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
@@ -30,6 +34,13 @@ public class Task06 {
         Random rnd = new Random(seed);
 
         // TODO: Пишите код здесь
-
+        List<Integer> arr = Stream.generate(() -> rnd.nextInt(11) - 5)
+                .limit(10).collect(Collectors.toList());
+        arr.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(e -> e.getValue() == 1)
+                .forEach(n -> System.out.println(n.getKey() + " -> " + arr.indexOf(n.getKey()) + " индекс"));
     }
 }
