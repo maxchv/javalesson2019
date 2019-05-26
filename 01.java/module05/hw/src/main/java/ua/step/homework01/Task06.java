aabc1a8f99fac0a350848474217e28234c35d0fc
Maxim <maxshaptala@gmail.com>
2019-05-26 23:53:40 +0300

    удалено решение

--- 01.java/module05/hw/src/main/java/ua/step/homework01/Task06.java
+++ 01.java/module05/hw/src/main/java/ua/step/homework01/Task06.java
@@ -2,10 +2,7 @@ package ua.step.homework01;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Random;
 
 /**
  * Задание: Заполнить массив на 10 элементов случайными числами
@@ -34,13 +31,5 @@ public class Task06 {
         Random rnd = new Random(seed);
 
         // TODO: Пишите код здесь
-        List<Integer> arr = Stream.generate(() -> rnd.nextInt(11) - 5)
-                .limit(10).collect(Collectors.toList());
-        arr.stream()
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet()
-                .stream()
-                .filter(e -> e.getValue() == 1)
-                .forEach(n -> System.out.println(n.getKey() + " -> " + arr.indexOf(n.getKey()) + " индекс"));
     }
 }
