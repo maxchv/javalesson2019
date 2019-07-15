f50757b78acff55aa3e1795fcb5349697a032fdd
Maxim <maxshaptala@gmail.com>
2019-07-15 18:05:14 +0300

    добавлен пример

---
+++ 01.java/module09/demo/jcfdemo/src/main/java/org/itstep/Application.java
@@ -0,0 +1,11 @@
+package org.itstep;
+
+
+import java.time.Instant;
+
+public class Application {
+    public static void main(String[] args) {
+        Instant start = Instant.now();
+        Instant end = Instant.now();
+    }
+}
