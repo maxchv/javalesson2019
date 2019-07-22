1d5773b9b0e65b762c7db348db54c5641859a128
Maxim <maxshaptala@gmail.com>
2019-07-22 17:56:06 +0300

    Добавлены примеры по рефлексии

--- 01.java/module09/demo/jcfdemo/src/main/java/org/itstep/Application.java
+++ 01.java/module09/demo/jcfdemo/src/main/java/org/itstep/Application.java
@@ -1,11 +1,19 @@
 package org.itstep;
 
+import lombok.extern.slf4j.Slf4j;
 
+import java.time.Duration;
 import java.time.Instant;
 
+@Slf4j
 public class Application {
+
     public static void main(String[] args) {
         Instant start = Instant.now();
-        Instant end = Instant.now();
+
+        // TODO: write your code here
+
+        Duration d = Duration.between(start, Instant.now());
+        log.info(String.format("Your code worked %s", d));
     }
 }
