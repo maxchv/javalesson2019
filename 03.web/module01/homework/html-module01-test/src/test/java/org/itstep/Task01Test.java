493d56629563f387ac3c9909f58fad2060d1cbad
Max Shaptala <maxshaptala@gmail.com>
2019-10-23 11:28:56 +0300

    added validator jcabi-w3c, move test to validate html to class TestBase

--- 03.web/module01/homework/html-module01-test/src/test/java/org/itstep/Task01Test.java
+++ 03.web/module01/homework/html-module01-test/src/test/java/org/itstep/Task01Test.java
@@ -1,29 +1,12 @@
 package org.itstep;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.regex.Pattern;
 
 import org.itstep.pages.Task01;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.support.PageFactory;
 
-import nu.validator.messages.MessageEmitter;
-import nu.validator.messages.MessageEmitterAdapter;
-import nu.validator.messages.TextMessageEmitter;
-import nu.validator.servlet.imagereview.ImageCollector;
-import nu.validator.source.SourceCode;
-import nu.validator.validation.SimpleDocumentValidator;
-import nu.validator.xml.SystemErrErrorHandler;
 
 public class Task01Test extends TestBase {
 
@@ -52,35 +35,5 @@ public class Task01Test extends TestBase {
     assertEquals("italic", homepage.getItalicParagraph().getCssValue("font-style"));
   }
 
-  public boolean validateHtml(Path path) throws Exception {
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-    SourceCode sourceCode = new SourceCode();
-    ImageCollector imageCollector = new ImageCollector(sourceCode);
-    boolean showSource = false;
-    MessageEmitter emitter = new TextMessageEmitter(out, false);
-    Pattern pattern = Pattern.compile("");
-    MessageEmitterAdapter errorHandler = new MessageEmitterAdapter(pattern, sourceCode, showSource, imageCollector, 0,
-        false, emitter);
-    
-    errorHandler.setErrorsOnly(true);
-
-    SimpleDocumentValidator validator = new SimpleDocumentValidator();
-    validator.setUpMainSchema("http://s.validator.nu/html5-rdfalite.rnc", new SystemErrErrorHandler());
-    validator.setUpValidatorAndParsers(errorHandler, true, false);
-    // validator.checkHtmlInputSource(new InputSource(in));
-    validator.checkHtmlFile(path.toFile(), true);
-    errorHandler.end("Ok", "Fails", "en");
-    byte[] buff = out.toByteArray();
-    String s = new String(buff, "utf-8");
-    System.out.println(s);
-    return 0 == errorHandler.getErrors();
-  }
-
-  @Test
-  public void validate() throws IOException, Exception {
-    var path = Paths.get(baseUrl.replace("file:///", ""), Task01.FILE_NAME);
-    if (!validateHtml(path)) {
-      fail("Not valid html");
-    }
-  }
+ 
 }
--- 03.web/module01/homework/html-module01-test/src/test/java/org/itstep/TestBase.java
+++ 03.web/module01/homework/html-module01-test/src/test/java/org/itstep/TestBase.java
@@ -1,12 +1,32 @@
 package org.itstep;
 
-import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import com.jcabi.w3c.ValidatorBuilder;
+
+import org.itstep.pages.Task01;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
+
+import nu.validator.messages.MessageEmitter;
+import nu.validator.messages.MessageEmitterAdapter;
+import nu.validator.messages.TextMessageEmitter;
+import nu.validator.servlet.imagereview.ImageCollector;
+import nu.validator.source.SourceCode;
+import nu.validator.validation.SimpleDocumentValidator;
+import nu.validator.xml.SystemErrErrorHandler;
 
 /**
  * Base class for all the JUnit-based test classes
@@ -43,6 +63,49 @@ public abstract class TestBase {
   public void tearDown() throws Throwable {
     if (driver != null) {
       driver.quit();
+    }
+  }
+
+  @Test
+  public void validateNvuHtml() throws Exception {
+    var path = getPath();
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    SourceCode sourceCode = new SourceCode();
+    ImageCollector imageCollector = new ImageCollector(sourceCode);
+    boolean showSource = false;
+    MessageEmitter emitter = new TextMessageEmitter(out, false);
+    Pattern pattern = Pattern.compile("");
+    MessageEmitterAdapter errorHandler = new MessageEmitterAdapter(pattern, sourceCode, showSource, imageCollector, 0,
+        false, emitter);
+
+    errorHandler.setErrorsOnly(true);
+
+    SimpleDocumentValidator validator = new SimpleDocumentValidator();
+    validator.setUpMainSchema("http://s.validator.nu/html5-rdfalite.rnc", new SystemErrErrorHandler());
+    validator.setUpValidatorAndParsers(errorHandler, true, false);
+    // validator.checkHtmlInputSource(new InputSource(in));
+    validator.checkHtmlFile(path.toFile(), true);
+    errorHandler.end("Ok", "Fails", "ru");
+
+    if (0 != errorHandler.getErrors()) {
+      byte[] buff = out.toByteArray();
+      String s = new String(buff, "utf-8");
+      fail("Not valid html " + s);
+    }
+  }
+
+  private Path getPath() {
+    return Paths.get(baseUrl.replace("file:///", ""), Task01.FILE_NAME);
+  }
+
+  @Test
+  public void validateW3c() throws IOException {
+    var path = getPath();
+    var result = new ValidatorBuilder().html().validate(Files.readString(path));
+    if (!result.valid()) {
+      var err = result.errors().stream().map(d -> "Line: " + d.line() + ": " + d.message() + ", " + d.explanation())
+          .collect(Collectors.joining("\n"));
+      fail("Not valid html: " + err);
     }
   }
 }
